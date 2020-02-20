91f5d73bf972332143583c12048d94311a8abf0d
Martin <westerlund_martin@hotmail.com>
2020-02-20 18:22:22 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const router = require('./router.js')
+const fs = require('fs')
+
+let results
+
+app.use('/', router)
+
+io.on('connection', (socket) => {
+
+    // Skickar en uppdatering av resultaten till den allmänna resultatsidan vid varje connection
+    socket.emit('update results', (results))
+    
+    // Lyssnar på eventet 'send results' som kommer från rapporteringssidorna
+    socket.on('send result', (data) => {
+        results.party1 += data.result1
+        results.party2 += data.result2
+        results.party3 += data.result3
+        
+        //Skriver över resultatet till fil
+        fs.writeFile('./results.json', JSON.stringify(results), (err) => {
+            if(err){
+                console.log(err)
+            } else {
+                console.log('Resultatfilen uppdaterad')
+            }
+        })
+        //Skickar iväg en uppdatering av resultaten till den allmänna resultatsidan
+        io.sockets.emit('update results', (results))
+    })
+})
+
+server.listen(8080, () => {
+    console.log('Server igång på 8080')
+
+    // Varje gång serven startar hämtar vi data från resultatfilen 
+    fs.readFile('./results.json', 'utf8', (err, data) => {
+        if(err){
+            console.error(err)
+        }
+        else{
+            results = JSON.parse(data)
+        }
+    })
+})
---
+++ router.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+
+let root = {
+    root: 'public/'
+}
+
+router.get('/', (req, res) => {
+    if(req.query.username === 'admin' && req.query.password === 'password'){
+        res.sendFile('index.html', {root})
+    } else{
+        res.sendFile('error.html', {root})
+    }
+   
+})
+
+router.get('/results', (req, res) => {
+    res.sendFile('results.html', {root})
+})
+
+
+module.exports = router
